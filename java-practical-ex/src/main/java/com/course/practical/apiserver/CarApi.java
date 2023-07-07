90dc22938c82cb166fefd08a145f1ee248362114
Mathan Mohan <mathan2006@gmail.com>
2023-07-07 10:20:14 +0530

    Add new attributes

--- java-practical-ex/src/main/java/com/course/practical/apiserver/CarApi.java
+++ java-practical-ex/src/main/java/com/course/practical/apiserver/CarApi.java
@@ -1,8 +1,12 @@
 package com.course.practical.apiserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,7 @@ import com.course.practical.service.CarService;
 
 public class CarApi {
 	
+	private static final Logger Log = LoggerFactory.getLogger(CarApi.class);
 	@Autowired
 	private CarService carService;
 	
@@ -22,5 +27,11 @@ public class CarApi {
 //	@RequestMapping(value="/random",method=RequestMethod.GET)
 	public Car random() {
 		return carService.generateCar();
+	}
+	
+	@PostMapping(value="/echo",consumes=MediaType.APPLICATION_JSON_VALUE)
+	public String echo(@RequestBody Car car) {
+		Log.info("Car is {}",car);
+		return car.toString();
 	}
 }
--- java-practical-ex/src/main/java/com/course/practical/entity/Car.java
+++ java-practical-ex/src/main/java/com/course/practical/entity/Car.java
@@ -1,10 +1,18 @@
 package com.course.practical.entity;
 
+import java.time.LocalDate;
+
 public class Car {
+	private boolean available;
 	private String brand;
 	private String color;
-	private String type;
 	
+	private int price;
+	private LocalDate releaseDate;
+	private String type;
+	public Car(){
+		
+	}
 	public Car(String brand, String color, String type) {
 		super();
 		this.brand = brand;
@@ -17,8 +25,20 @@ public class Car {
 	public String getColor() {
 		return color;
 	}
+	public int getPrice() {
+		return price;
+	}
+	public LocalDate getReleaseDate() {
+		return releaseDate;
+	}
 	public String getType() {
 		return type;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
 	}
 	public void setBrand(String brand) {
 		this.brand = brand;
@@ -26,12 +46,19 @@ public class Car {
 	public void setColor(String color) {
 		this.color = color;
 	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public void setReleaseDate(LocalDate releaseDate) {
+		this.releaseDate = releaseDate;
+	}
 	public void setType(String type) {
 		this.type = type;
 	}
 	@Override
 	public String toString() {
-		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + "]";
+		return "Car [brand=" + brand + ", color=" + color + ", type=" + type + ", price=" + price + ", available="
+				+ available + ", releaseDate=" + releaseDate + "]";
 	}
 	
 }
